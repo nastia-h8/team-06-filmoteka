77ab34a5aef1144b7c4148c9f78b371de941d6e7
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-26 20:51:05 +0200

    update scss

--- src/js/registration-modal.js
+++ src/js/registration-modal.js
@@ -9,15 +9,20 @@ logInButtonRef.addEventListener('click', onLoginBtnClick);
 // ===============================================================
 function onLoginBtnClick() {
   modalWindow.classList.remove('invis');
-  rowRef.addEventListener('click', onCloseModalBtn);
   window.addEventListener('keydown', modalCloseOnEscPress);
+  modalWindow.addEventListener('click', onCloseModalBtn);
 }
 // ===============================================================
+// ===============================================================
 function onCloseModalBtn(event) {
-  if (event.target.classList.contains('fa-solid')) {
+  if (
+    event.target.classList.contains('fa-solid') ||
+    event.target === event.currentTarget
+  ) {
     modalWindow.classList.add('invis');
+    window.removeEventListener('keydown', modalCloseOnEscPress);
+    modalWindow.removeEventListener('click', onCloseModalBtn);
   }
-  console.log('message');
 }
 // ===============================================================
 function modalCloseOnEscPress(event) {
