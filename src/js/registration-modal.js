91155004e5df00476b1977ef075a32f0892e8554
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-26 11:08:31 +0200

    add "Escape" eventListener

--- src/js/registration-modal.js
+++ src/js/registration-modal.js
@@ -1,5 +1,6 @@
 const logInButtonRef = document.querySelector('.auth-btn');
 const modalWindow = document.querySelector('.container__form');
+const rowRef = document.querySelector('.row');
 const backDropRef = document.querySelector('.backdrop');
 const closeModalBtnRef = document.querySelector('.fa-solid');
 // ===============================================================
@@ -8,13 +9,20 @@ logInButtonRef.addEventListener('click', onLoginBtnClick);
 // ===============================================================
 function onLoginBtnClick() {
   modalWindow.classList.remove('invis');
-  window.addEventListener('click', onCloseModalBtn);
+  rowRef.addEventListener('click', onCloseModalBtn, { once: true });
+  window.addEventListener('keydown', modalCloseOnEscPress, { once: true });
 }
 // ===============================================================
 function onCloseModalBtn(event) {
   if (event.target.classList.contains('fa-solid')) {
     modalWindow.classList.add('invis');
-    console.log(event);
   }
+  console.log('event.currentTarget', event.currentTarget);
+  console.log('event.target', event.target);
+}
+// ===============================================================
+function modalCloseOnEscPress(event) {
+  if (event.code !== 'Escape') return;
+  modalWindow.classList.add('invis');
 }
 // ===============================================================
