1bca352289e44989511dae1f9f01851241a15520
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-28 00:17:30 +0200

    update reg.window

--- src/js/registration-modal.js
+++ src/js/registration-modal.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const logInButtonRef = document.querySelector('.auth-btn');
 const modalWindow = document.querySelector('.container__form');
 const rowRef = document.querySelector('.row');
@@ -5,7 +7,11 @@ const backDropRef = document.querySelector('.backdrop');
 const closeModalBtnRef = document.querySelector('.fa-solid');
 const checkBoxRef = document.querySelector('.form-check-input');
 const formButtonRef = document.querySelector('.btn-primary');
-const formRef = document.querySelector('form');
+const formRef = document.querySelector('.form');
+const firstPassInputRef = document.querySelector('#firstPass');
+const repeatPassInputRef = document.querySelector('#repeatedPass');
+const userNameInputRef = document.querySelector('#userName');
+const userEmailInputRef = document.querySelector('#userEmail');
 // ===============================================================
 formButtonRef.disabled = true;
 // ===============================================================
@@ -16,8 +22,27 @@ function onLoginBtnClick() {
   modalWindow.classList.remove('invis');
   window.addEventListener('keydown', modalCloseOnEscPress);
   modalWindow.addEventListener('click', onCloseModalBtn);
+  formRef.addEventListener('submit', onFormSubmit);
+  console.log('formRef: ', formRef);
 }
 // ===============================================================
+function onFormSubmit(e) {
+  e.preventDefault();
+  if (firstPassInputRef.value !== repeatPassInputRef.value) {
+    Notiflix.Notify.failure('Passwords do not match each others');
+  }
+  {
+    const userName = userNameInputRef.value;
+    const userEmail = userEmailInputRef.value;
+    const userPassword = firstPassInputRef.value;
+    const userData = {
+      name: userName,
+      email: userEmail,
+      password: userPassword,
+    };
+    console.log(userData);
+  }
+}
 // ===============================================================
 function onCloseModalBtn(event) {
   if (
@@ -37,13 +62,13 @@ function modalCloseOnEscPress(event) {
 // ===============================================================
 function onToggle(event) {
   if (this.checked) {
-    console.log('Checkbox is checked..');
+    // console.log('Checkbox is checked..');
     formButtonRef.disabled = false;
     formButtonRef.style.border = '1px black solid';
   } else {
     console.log('Checkbox is not checked..');
     formButtonRef.disabled = true;
-    formButtonRef.style.border = '1px var(--tertiary-color) solid';
+    // formButtonRef.style.border = '1px var(--tertiary-color) solid';
   }
 }
 // ===============================================================
