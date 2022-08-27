c46abcffe7b79a8f074159dc45818803af338fca
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-27 23:42:15 +0200

    update reg.window

--- src/js/registration-modal.js
+++ src/js/registration-modal.js
@@ -3,9 +3,14 @@ const modalWindow = document.querySelector('.container__form');
 const rowRef = document.querySelector('.row');
 const backDropRef = document.querySelector('.backdrop');
 const closeModalBtnRef = document.querySelector('.fa-solid');
+const checkBoxRef = document.querySelector('.form-check-input');
+const formButtonRef = document.querySelector('.btn-primary');
+const formRef = document.querySelector('form');
 // ===============================================================
+formButtonRef.disabled = true;
 // ===============================================================
 logInButtonRef.addEventListener('click', onLoginBtnClick);
+checkBoxRef.addEventListener('change', onToggle);
 // ===============================================================
 function onLoginBtnClick() {
   modalWindow.classList.remove('invis');
@@ -28,5 +33,17 @@ function onCloseModalBtn(event) {
 function modalCloseOnEscPress(event) {
   if (event.code !== 'Escape') return;
   modalWindow.classList.add('invis');
+}
+// ===============================================================
+function onToggle(event) {
+  if (this.checked) {
+    console.log('Checkbox is checked..');
+    formButtonRef.disabled = false;
+    formButtonRef.style.border = '1px black solid';
+  } else {
+    console.log('Checkbox is not checked..');
+    formButtonRef.disabled = true;
+    formButtonRef.style.border = '1px var(--tertiary-color) solid';
+  }
 }
 // ===============================================================
