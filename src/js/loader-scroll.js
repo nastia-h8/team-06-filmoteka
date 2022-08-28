a66202873e0e9d6c46a385641cd5102bfe9d9cb2
Antonyly <antonlyzun@gmail.com>
2022-08-28 20:54:41 +0300

    +

--- src/js/loader-scroll.js
+++ src/js/loader-scroll.js
@@ -16,24 +16,40 @@ delBtn();
 window.addEventListener('scroll', () => {
 
             if(window.scrollY < 100){
-                delBtn()
+                delBtn();
                 return;
             } else if (window.scrollY >= 100) {
-            backToTopButton.classList.add('block');
-            backToTopButton.classList.remove('none');
+                addBtn();
                 return;
             }
         // console.log(Math.floor(window.scrollY));
 });
+window.addEventListener('scroll', closeOpenbtn)
+
+        export function closeOpenbtn() {
+            if(window.scrollY < 100){
+                delBtn();
+                return;
+            } else if (window.scrollY >= 100) {
+                addBtn();
+                return;
+                };
+        };
+        // console.log(Math.floor(window.scrollY));
+
 export function delBtn() {
     backToTopButton.classList.add('none');
     backToTopButton.classList.remove('block');
-}
+};
+export function addBtn() {
+    backToTopButton.classList.add('block');
+    backToTopButton.classList.remove('none');
+};
         
 window.onload = function () {
-            document.body.classList.add('loaded_hiding');
-            window.setTimeout(function () {
-                document.body.classList.add('loaded');
-                document.body.classList.remove('loaded_hiding');
-            }, 500);
-}
+    document.body.classList.add('loaded_hiding');
+    window.setTimeout(function () {
+        document.body.classList.add('loaded');
+        document.body.classList.remove('loaded_hiding');
+    }, 500);
+};
