c7e9e96d98838cabb429a54daea0699540e3037c
Serhii Kaushnian <skaushnjan@gmail.com>
2022-08-29 10:43:24 +0200

    update login window features

--- src/js/registration-modal.js
+++ src/js/registration-modal.js
@@ -22,7 +22,6 @@ const auth = getAuth(firebaseConfig);
 // ===============================================================
 const user = auth.currentUser;
 // ===============================================================
-
 const logInButtonRef = document.querySelector('.auth-btn');
 const logOutButtonRef = document.querySelector('.logout__btn');
 const modalWindow = document.querySelector('.container__form');
@@ -30,7 +29,8 @@ const rowRef = document.querySelector('.row');
 const backDropRef = document.querySelector('.backdrop');
 const closeModalBtnRef = document.querySelector('.fa-solid');
 const checkBoxRef = document.querySelector('.form-check-input');
-const formButtonRef = document.querySelector('.btn-primary');
+const formButtonSignUpRef = document.querySelector('.btn__signup');
+const formButtonLogInRef = document.querySelector('.btn__login');
 const formRef = document.querySelector('.form');
 let firstPassInputRef = document.querySelector('#firstPass');
 const repeatPassInputRef = document.querySelector('#repeatedPass');
@@ -40,7 +40,7 @@ const loginLinkRef = document.querySelector('.login__link');
 const libraryLinkRef = document.querySelector('.library-link');
 // ===============================================================
 libraryLinkRef.addEventListener('click', onLibraryLinkClick);
-formButtonRef.disabled = true;
+formButtonSignUpRef.disabled = true;
 // ===============================================================
 function checkLocalStorage() {
   if (localStorage.getItem('userData')) {
@@ -51,29 +51,27 @@ function checkLocalStorage() {
 }
 checkLocalStorage();
 // ===============================================================
-
 // ===============================================================
 logInButtonRef.addEventListener('click', onLoginBtnClick);
 checkBoxRef.addEventListener('change', onToggle);
+logOutButtonRef.addEventListener('click', logOutHandler);
 // ===============================================================
 function onLoginBtnClick() {
   modalWindow.classList.remove('invis');
   window.addEventListener('keydown', modalCloseOnEscPress);
   modalWindow.addEventListener('click', onCloseModalBtn);
-  loginLinkRef.addEventListener('click', onLoginLinkClick);
+  formButtonLogInRef.addEventListener('click', onLoginPageSubmit);
   formRef.addEventListener('submit', onFormSubmit);
+  onToggle();
 }
 // ===============================================================
 function onFormSubmit(e) {
   e.preventDefault();
-  if (firstPassInputRef.value !== repeatPassInputRef.value) {
-    Notiflix.Notify.failure('Passwords do not match each others');
-  } else if (firstPassInputRef.value.length < 6) {
+  if (firstPassInputRef.value.length < 6) {
     Notiflix.Notify.failure('Password should be at least 6 characters');
   } else if (userEmailInputRef.value.length === 0) {
     Notiflix.Notify.failure('Please enter Your email address');
   } else {
-    const userName = userNameInputRef.value;
     const userEmail = userEmailInputRef.value;
     const userPassword = firstPassInputRef.value;
     createAccount(auth, userEmail, userPassword);
@@ -98,12 +96,12 @@ function modalCloseOnEscPress(event) {
   modalWindow.classList.add('invis');
 }
 // ===============================================================
-function onToggle(event) {
-  if (this.checked) {
-    formButtonRef.disabled = false;
-    formButtonRef.style.border = '1px black solid';
+function onToggle() {
+  if (checkBoxRef.checked) {
+    formButtonSignUpRef.disabled = false;
+    formButtonSignUpRef.style.border = '1px black solid';
   } else {
-    formButtonRef.disabled = true;
+    formButtonSignUpRef.disabled = true;
   }
 }
 // ===============================================================
@@ -126,52 +124,6 @@ async function createAccount(auth, email, password) {
 }
 
 // ===============================================================
-function onLoginLinkClick(e) {
-  e.preventDefault();
-  const loginMarkUp = `	<div class="row">
-		<div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
-			<p class="text-center">Log In</p>
-			<div class="close__icon">
-				<i class="fa-solid fa-circle-xmark"></i>
-			</div>
-			<form class="mx-1 mx-md-4 form form__login">
-
-				<div class="form__input">
-					<i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-					<div class="form-outline flex-fill mb-0">
-					<input type="email" id="userEmail" class="form-control" placeholder=" " />
-						<label class="form-label" for="userEmail">Your Email</label>
-						</div>
-						</div>
-						
-						<div class="form__input">
-						<i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-						<div class="form-outline flex-fill mb-0">
-						<input type="password" id="firstPass" class="form-control" placeholder=" " />
-						<label class="form-label" for="firstPass">Password</label>
-						</div>
-						</div>
-						<button type="submit" class="btn btn-primary btn-lg">Log In</button>
-						<p class="signup"><span class="accent">Changed Yor mind?</br></span> Procced to <a class="login__link">Sign Up Page</a> </p>
-						</form>
-						</div>
-						<div class="img__container">
-						
-						<img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image">
-						
-		</div>
-	</div>`;
-  modalWindow.innerHTML = '';
-  modalWindow.insertAdjacentHTML('afterbegin', loginMarkUp);
-  userEmailInputRef = document.querySelector('#userEmail');
-  firstPassInputRef = document.querySelector('#firstPass');
-  const formLoginRef = document.querySelector('.form__login');
-  const signUpPageRef = document.querySelector('.signup');
-  signUpPageRef.addEventListener('click', backToSignUpPage);
-  formRef.removeEventListener('submit', onFormSubmit);
-  formLoginRef.addEventListener('submit', onLoginPageSubmit);
-}
 
 // ===============================================================
 function onLoginPageSubmit(e) {
@@ -183,7 +135,6 @@ function onLoginPageSubmit(e) {
     'userData',
     JSON.stringify({ userEmail, userPassword })
   );
-
   loginIntoAccount(auth, userEmail, userPassword);
   formRef.reset();
   modalWindow.classList.add('invis');
@@ -197,7 +148,7 @@ async function loginIntoAccount(auth, email, password) {
       email,
       password
     );
-    logOutButtonRef.addEventListener('click', logOutHandler);
+    Notiflix.Notify.success('You are In');
     libraryLinkRef.removeEventListener('click', onLibraryLinkClick);
   } catch (error) {
     console.log(error);
@@ -208,102 +159,23 @@ async function loginIntoAccount(auth, email, password) {
 }
 
 // ===============================================================
-
-// onAuthStateChanged((auth, user) => {
-//   console.log(user);
-//   if (user) {
-//     const headerNavContainer = document.querySelector('.nav');
-//     headerNavContainer.insertAdjacentHTML(
-//       'afterbegin',
-//       `<button class="auth-btn louout__button" type="button">Log out</button>`
-//     );
-//   } else {
-//   }
-// });
-
 // ===============================================================
 function logOutHandler() {
-  signOut(auth)
-    .then(() => {
-      Notiflix.Notify.success('Sign-out successful.');
-      libraryLinkRef.addEventListener('click', onLibraryLinkClick);
-      localStorage.removeItem('userData');
-    })
-    .catch(error => {
-      Notiflix.Notify.warning('Sign-out unsuccessful.');
-    });
+  if (localStorage.getItem('userData')) {
+    signOut(auth)
+      .then(() => {
+        Notiflix.Notify.success('Sign-out successful.');
+        libraryLinkRef.addEventListener('click', onLibraryLinkClick);
+        localStorage.removeItem('userData');
+      })
+      .catch(error => {
+        Notiflix.Notify.warning('Sign-out unsuccessful.');
+      });
+  }
 }
 // ===============================================================
 function onLibraryLinkClick(e) {
   e.preventDefault();
   Notiflix.Notify.warning('To use "Mi Library" page. First You need to login');
-}
-// ===============================================================
-function backToSignUpPage(e) {
-  e.preventDefault();
-  const signUpMarkUp = `	<div class="row">
-		<div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
-			<p class="text-center">Sign up</p>
-			<div class="close__icon">
-				<i class="fa-solid fa-circle-xmark"></i>
-			</div>
-			<form class="mx-1 mx-md-4 form">
-
-				<div class="form__input">
-					<i class="fas fa-user fa-lg me-3 fa-fw"></i>
-					<div class="form-outline flex-fill mb-0">
-						<input type="text" id="userName" class="form-control" placeholder=" " />
-						<label class="form-label" for="userName">Your Name</label>
-					</div>
-				</div>
-
-				<div class="form__input">
-					<i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-					<div class="form-outline flex-fill mb-0">
-						<input type="email" id="userEmail" class="form-control" placeholder=" " />
-						<label class="form-label" for="userEmail">Your Email</label>
-					</div>
-				</div>
-
-				<div class="form__input">
-					<i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-					<div class="form-outline flex-fill mb-0">
-						<input type="password" id="firstPass" class="form-control" placeholder=" " />
-						<label class="form-label" for="firstPass">Password</label>
-					</div>
-				</div>
-
-				<div class="form__input">
-					<i class="fas fa-key fa-lg me-3 fa-fw"></i>
-					<div class="form-outline flex-fill mb-0">
-						<input type="password" id="repeatedPass" class="form-control" placeholder=" " />
-						<label class="form-label" for="repeatedPass">Repeat your password</label>
-					</div>
-				</div>
-
-				<div class="form-check d-flex justify-content-center mb-5">
-					<input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
-					<label class="form-check-label" for="form2Example3">
-						I agree all statements in <a href="#!">Terms of service</a>
-					</label>
-				</div>
-
-
-				<button type="submit" class="btn btn-primary btn-lg">Register</button>
-				<p class="login__link"><span class="accent">Already have an account?<br></span><br>Procced to <a class="login__link">Login Page</a> </p>
-			</form>
-
-		</div>
-		<div class="img__container">
-
-			<img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image">
-
-		</div>
-	</div>`;
-  modalWindow.innerHTML = '';
-  modalWindow.insertAdjacentHTML('afterbegin', signUpMarkUp);
-  modalWindow.addEventListener('click', onCloseModalBtn);
-  loginLinkRef.addEventListener('click', onLoginLinkClick);
 }
 // ===============================================================
