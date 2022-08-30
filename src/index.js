d25797c27e6251030e5debcc44ceec6137d1969d
Anatolii <anatoly.tomnyak@gmail.com>
2022-08-30 14:10:46 +0300

    update modal team

--- src/index.js
+++ src/index.js
@@ -19,4 +19,4 @@ import './js/fetch-films-by-search';
 import './js/submit-form';
 import './js/renderCardsFilms';
 import './js/pagination-test';
-import './js/footer-modal-team';
+import './js/modal-team';
